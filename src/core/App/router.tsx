ea824a886c800fd2d7ebda3f1135b52cea831f3b
GrzegorzJendernal <g.jendernal@gmail.com>
2023-05-24 20:58:50 +0200

    added dev error page

--- src/core/App/router.tsx
+++ src/core/App/router.tsx
@@ -6,14 +6,17 @@ export const router = createHashRouter([
     path: "/",
     element: <Header />,
     children: [
-      {
-        path: "/movies",
-        element: <div>filmy</div>,
-      },
-      {
-        path: "/people",
-        element: <div>ludzie</div>,
-      },
+	    {errorElement: <div>error</div>,
+	    children: [
+		    {
+			    path: "/movies",
+			    element: <div>filmy</div>,
+		    },
+		    {
+			    path: "/people",
+			    element: <div>ludzie</div>,
+		    },
+	    ]},
     ],
   },
 ]);
