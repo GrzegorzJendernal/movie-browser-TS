68559b81d3b7052c84e29341c435144ceaddbeed
GrzegorzJendernal <g.jendernal@gmail.com>
2023-05-25 22:01:20 +0200

    refactored Main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./core/App/App.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./core/router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 );
