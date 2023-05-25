4941c1fae22bf55ad1a33fd795224de49dcf7116
GrzegorzJendernal <g.jendernal@gmail.com>
2023-05-25 22:01:11 +0200

    fixed App

--- src/core/App/App.tsx
+++ src/core/App/App.tsx
@@ -2,15 +2,16 @@ import { GlobalStyle } from "./GlobalStyle";
 import { Normalize } from "styled-normalize";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../theme";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import Header from "../../common/Header/Header";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Normalize />
       <GlobalStyle />
-      <RouterProvider router={router} />
+      <Header/>
+	    <Outlet/>
     </ThemeProvider>
   );
 };
