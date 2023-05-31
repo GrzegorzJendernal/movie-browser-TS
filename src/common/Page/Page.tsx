1288f623124e6877f346641fb13f08c4f9538927
GrzegorzJendernal <g.jendernal@gmail.com>
2023-05-31 22:10:03 +0200

    reformat with priettier

--- src/common/Page/Page.tsx
+++ src/common/Page/Page.tsx
@@ -2,15 +2,15 @@ import { ReactNode } from "react";
 import { Container, Heading } from "./page.styled";
 
 interface PageProps {
-	title?: string;
-	content?: ReactNode;
+  title?: string;
+  content?: ReactNode;
 }
 
-const Page = ({title, content}: PageProps) => (
-	<Container>
-		{!!title && <Heading>{title}</Heading>}
-	 {content}
-	</Container>
+const Page = ({ title, content }: PageProps) => (
+  <Container>
+    {!!title && <Heading>{title}</Heading>}
+    {content}
+  </Container>
 );
 
 export default Page;
