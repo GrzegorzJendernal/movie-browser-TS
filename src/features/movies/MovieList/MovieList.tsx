e8d9403effd8d7da0d7ab46da55b9536139bd7c6
GrzegorzJendernal <g.jendernal@gmail.com>
2023-05-31 22:09:21 +0200

    added MovieList

---
+++ src/features/movies/MovieList/MovieList.tsx
@@ -0,0 +1,19 @@
+import Tile from "../../../common/Tile/Tile";
+import { Container } from "./movieList.styled";
+
+const MovieList = () => {
+	return (
+		<Container>
+			<Tile/>
+			<Tile/>
+			<Tile/>
+			<Tile/>
+			<Tile/>
+			<Tile/>
+			<Tile/>
+			<Tile/>
+		</Container>
+	);
+};
+
+export default MovieList;
