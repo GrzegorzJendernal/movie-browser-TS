d3c911ebd8430529f8cf64e41835153ad8ea4b3a
GrzegorzJendernal <g.jendernal@gmail.com>
2023-06-01 21:44:28 +0200

    fixed styles Tile

--- src/common/Tile/tile.styled.ts
+++ src/common/Tile/tile.styled.ts
@@ -1,38 +1,41 @@
 import styled from "styled-components";
 import noPoster from "./noMoviePoster.svg";
-import {ReactComponent as Star} from "./star.svg";
+import { ReactComponent as Star } from "./star.svg";
 
-export const StyledTile = styled.div`
-  width: 324px;
-  height: 650px;
-	background-color: ${({ theme }) => theme.colors.base};
-	box-shadow: ${({ theme }) => theme.boxShadow};
-	border-radius: 5px;
-	padding: 16px;
+export const StyledTile = styled.section`
+ display: flex;
+	flex-direction: column;
+	width: 100%;
+  background-color: ${({ theme }) => theme.colors.base};
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  border-radius: 5px;
+  padding: 16px;
 `;
 
 export const Poster = styled.div`
   width: 292px;
   height: 434px;
-	margin-bottom: 16px;
-	border-radius: 5px;
-	background-image: url(${noPoster});
-	background-repeat: no-repeat;
-	background-size: 100%;
+  margin-bottom: 16px;
+  border-radius: 5px;
+  background-image: url(${noPoster});
+  background-repeat: no-repeat;
+  background-size: 100%;
 `;
 
-export const Description = styled.article`
-display: flex;
-	gap: 8px;
-	flex-direction: column;
+export const Info = styled.article`
+  display: grid;
+	grid-gap: 8px;
+	grid-template-columns: 1fr;
+	align-content: stretch;
 `;
 
 export const Title = styled.h2`
   font-weight: 500;
   font-size: 22px;
   line-height: 130%;
-	color: ${({ theme }) => theme.colors.primary};
-	margin: 0;
+  color: ${({ theme }) => theme.colors.primary};
+  margin: 0;
+	word-wrap: normal;
 `;
 
 export const Date = styled.span`
@@ -40,42 +43,46 @@ export const Date = styled.span`
   font-size: 16px;
   line-height: 150%;
   color: ${({ theme }) => theme.colors.secondary};
+	margin: 0;
 `;
 
-export const Genres = styled.div`
-display: flex;
-	flex-direction: row;
-	gap: 8px;
+export const Tags = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+	margin: 0;
+  flex-wrap: wrap;
 `;
 
-export const GenreTile = styled.div`
+export const Tag = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   padding: 8px 16px;
-	background-color: ${({ theme }) => theme.colors.disabled};
-	color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.disabled};
+  color: ${({ theme }) => theme.colors.primary};
   font-weight: 400;
   font-size: 14px;
   line-height: 140%;
   border-radius: 5px;
 `;
 
-export const Notes = styled.div`
-display: flex;
-	flex-direction: row;
-	gap: 12px;
+export const Rates = styled.div`
+  display: flex;
+  gap: 12px;
+	margin-top: auto;
+	align-items: center;
 `;
 
 export const StyledStar = styled(Star)`
-width: 24px;
-height: auto;
+  width: 24px;
+  height: auto;
 `;
 
 export const Note = styled.span`
-font-weight: 600;
-font-size: 16px;
-line-height: 150%;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 150%;
   color: ${({ theme }) => theme.colors.primary};
 `;
 
@@ -83,5 +90,5 @@ export const Votes = styled.span`
   font-weight: 400;
   font-size: 16px;
   line-height: 150%;
-	color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.secondary};
 `;
