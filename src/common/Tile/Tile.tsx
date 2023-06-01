5c9acd84bfeede9afa61a6bcc00c106f1a5d099b
GrzegorzJendernal <g.jendernal@gmail.com>
2023-06-01 21:44:43 +0200

    added props to Tile

--- src/common/Tile/Tile.tsx
+++ src/common/Tile/Tile.tsx
@@ -1,38 +1,28 @@
-import {
-	Date,
-	Description,
-	Genres,
-	GenreTile,
-	Note,
-	Notes,
-	Poster,
-	StyledStar,
-	StyledTile,
-	Title, Votes
-} from "./tile.styled";
+import { Date, Info, Tags, Tag, Note, Rates, Poster, StyledStar, StyledTile, Title, Votes } from "./tile.styled";
 
-const Tile = () => {
+interface TileProps {
+	title: string;
+	date: number;
+	genres?: string[];
+	note: number;
+	votes: number;
+}
+const Tile = ({title, date, genres, note, votes}: TileProps) => {
   return (
     <StyledTile>
       <Poster />
-	    <Description>
-<Title>
-	Tytuł filmu
-</Title>
-<Date>
-	2020
-</Date>
-<Genres>
-	<GenreTile>Action</GenreTile>
-	<GenreTile>Drama</GenreTile>
-	<GenreTile>Horror</GenreTile>
-</Genres>
-		    <Notes>
-<StyledStar/>
-<Note>7,5</Note>
-<Votes>38 votes</Votes>
-		    </Notes>
-	    </Description>
+      <Info>
+        <Title>{title}</Title>
+        <Date>{date}</Date>
+        <Tags>
+	        {!!genres && genres.map((genre) => <Tag key={genre}>{genre}</Tag>)}
+        </Tags>
+        <Rates>
+          <StyledStar />
+          <Note>{note}</Note>
+          <Votes>{votes} votes</Votes>
+        </Rates>
+      </Info>
     </StyledTile>
   );
 };
