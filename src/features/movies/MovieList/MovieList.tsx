060449c873445db45a465a2b8fe1b06d659884ea
GrzegorzJendernal <g.jendernal@gmail.com>
2023-06-01 21:43:08 +0200

    customized MovieList

--- src/features/movies/MovieList/MovieList.tsx
+++ src/features/movies/MovieList/MovieList.tsx
@@ -4,14 +4,61 @@ import { Container } from "./movieList.styled";
 const MovieList = () => {
 	return (
 		<Container>
-			<Tile/>
-			<Tile/>
-			<Tile/>
-			<Tile/>
-			<Tile/>
-			<Tile/>
-			<Tile/>
-			<Tile/>
+			<Tile
+			title={"Tytuł filmu"}
+			date={2020}
+			genres={["Action", "Thriller", "Horror"]}
+			note={7.5}
+			votes={38}
+			/>
+			<Tile
+				title={"Abrakadabra"}
+				date={2010}
+				note={9}
+				votes={999}
+			/>
+			<Tile
+				title={"Tytuł filmu, który jest bardzo długi, żeby zobaczyć co się będzie działo"}
+				date={2020}
+				genres={["Action", "Thriller", "Horror", "kjshjcsdj", "Action", "Thriller", "Horror", "kjshjcsdj"]}
+				note={99}
+				votes={125548896}
+			/>
+			<Tile
+				title={"Tytuł filmu"}
+				date={2020}
+				genres={["Action", "Thriller", "Horror"]}
+				note={7.5}
+				votes={38}
+			/>
+			<Tile
+				title={"Tytuł filmu"}
+				date={2020}
+				genres={["Action", "Thriller", "Horror"]}
+				note={7.5}
+				votes={38}
+			/>
+			<Tile
+				title={"Tytuł filmu"}
+				date={2020}
+				genres={["Action", "Thriller", "Horror"]}
+				note={7.5}
+				votes={38}
+			/>
+			<Tile
+				title={"Tytuł filmu"}
+				date={2020}
+				genres={["Action", "Thriller", "Horror"]}
+				note={7.5}
+				votes={38}
+			/>
+			<Tile
+				title={"Tytuł filmu"}
+				date={2020}
+				genres={["Action", "Thriller", "Horror"]}
+				note={7.5}
+				votes={38}
+			/>
 		</Container>
 	);
 };
