3bf23875f35ceef654eff3072207925052529c5b
GrzegorzJendernal <g.jendernal@gmail.com>
2023-06-01 21:43:30 +0200

    updated styles of MovieList

--- src/features/movies/MovieList/movieList.styled.ts
+++ src/features/movies/MovieList/movieList.styled.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-	margin-top: 24px;
-display: grid;
-	grid-template-columns: 1fr 1fr 1fr 1fr;
-	grid-gap: 24px;
+  margin-top: 24px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 24px;
 `;
