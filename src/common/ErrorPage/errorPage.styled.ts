10d8e88cd587dc0cf7d1b4290bfb88480b484e27
GrzegorzJendernal <g.jendernal@gmail.com>
2023-06-11 21:43:22 +0200

    added @media

--- src/common/ErrorPage/errorPage.styled.ts
+++ src/common/ErrorPage/errorPage.styled.ts
@@ -6,48 +6,76 @@ export const Container = styled.section`
   display: flex;
   flex-direction: column;
   gap: 24px;
-	justify-content: center;
-	align-items: center;
+  justify-content: center;
+  align-items: center;
   color: ${({ theme }) => theme.colors.primary};
-	margin-top: 180px;
+  margin-top: 180px;
+  text-align: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    margin-top: 90px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    margin-top: 60px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tiny}) {
+    margin-top: 24px;
+  }
 `;
 
 export const Sign = styled(Danger)`
   width: 120px;
   height: auto;
-`;
 
-export const Heading = styled.h2`
-  margin: 0;
-  font-weight: 600;
-  font-size: 36px;
-  line-height: 1.2;
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    width: 90px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    width: 60px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tiny}) {
+    width: 30px;
+  }
 `;
 
 export const Paragraph = styled.p`
   font-weight: 500;
   font-size: 22px;
   line-height: 1.3;
-	text-align: center;
+  text-align: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    font-size: 20px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 16px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.tiny}) {
+    font-size: 14px;
+  }
 `;
 
 export const ToHomepageLink = styled(Link)`
   padding: 16px 24px;
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   font-style: normal;
   font-weight: 700;
   font-size: 14px;
   line-height: 1.36;
   color: ${({ theme }) => theme.colors.base};
-	background-color: ${({ theme }) => theme.colors.special};
+  background-color: ${({ theme }) => theme.colors.special};
   border-radius: 5px;
-	text-decoration: none;
-	
-	&:hover {
-		filter: brightness(1.3);
-	}
-	
-	&:active {
-		filter: brightness(1.6);
-	}
+  text-decoration: none;
+
+  &:hover {
+    filter: brightness(1.3);
+  }
+
+  &:active {
+    filter: brightness(1.6);
+  }
 `;
