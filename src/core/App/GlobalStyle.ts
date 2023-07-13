1decaf7565d6ff505d87b341a692ff288f568b82
GrzegorzJendernal <g.jendernal@gmail.com>
2023-07-13 20:35:06 +0200

    removed pre-wrap

--- src/core/App/GlobalStyle.ts
+++ src/core/App/GlobalStyle.ts
@@ -14,6 +14,5 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         font-family: 'Poppins', sans-serif;
       	background-color: ${({ theme }) => theme.colors.background};
-	    	white-space: pre-wrap;
     }
 `;
