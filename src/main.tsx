f140f7ed33b513466e4d793345e220351bec613e
GrzegorzJendernal <g.jendernal@gmail.com>
2023-07-13 21:26:40 +0200

    added TanstackQuery

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./core/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 60 * 5,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+	  <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+		  <ReactQueryDevtools/>
+	  </QueryClientProvider>
   </React.StrictMode>,
 );
