f12054e07bc380c5bffe1db5f820a8f4d0e43e34
GrzegorzJendernal <g.jendernal@gmail.com>
2023-07-14 22:46:43 +0200

    added interfaces and function to getMovieList

--- src/features/movies/MovieList/getMovieList.ts
+++ src/features/movies/MovieList/getMovieList.ts
@@ -1,10 +1,40 @@
+// eslint-disable-next-line import/named
 import axios from "axios";
 
-export const getMovieList = async () => {
+interface Movie {
+	adult: boolean,
+	backdrop_path: string,
+	genre_ids: number[],
+	id: number,
+	original_language: string,
+	original_title: string,
+	overview: string,
+	popularity: number,
+	poster_path: string,
+	release_date: string,
+	title: string,
+	video: boolean,
+	vote_average: number,
+	vote_count: number,
+}
+
+interface MovieListData {
+	page: number,
+	results: Movie[],
+	total_pages: number,
+	total_results: number,
+}
+
+interface Params {
+	language: string,
+	page: string,
+}
+
+const getData = async (url: string, params?: Params) => {
 	const options = {
 		method: 'GET',
-		url: 'https://api.themoviedb.org/3/movie/popular',
-		params: {language: 'en-US', page: '1'},
+		url: url,
+		params: params,
 		headers: {
 			accept: 'application/json',
 			Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDUzMDk1NWFjZWRjMjQ3OGNiOTJhMTk0YWZkNDVhYiIsInN1YiI6IjYzZTdlYjNhNjNhYWQyMDA4NTg3NTIzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oxv_MlyvRTPzhd8zo1ddZvSkPgbC4gip6lYNwMbUYvs'
@@ -15,11 +45,4 @@ export const getMovieList = async () => {
 	return response.data
 };
 
-// axios
-// 	.request(options)
-// 	.then(function (response) {
-// 		console.log(response.data);
-// 	})
-// 	.catch(function (error) {
-// 		console.error(error);
-// 	});
+export const getMovieList = (): Promise<MovieListData> => getData('https://api.themoviedb.org/3/movie/popular', {language: "us-en", page: "1"});
