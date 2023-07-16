e6adf1d2d3c66eb7ce08adaf7bd260b9ca161c1a
GrzegorzJendernal <g.jendernal@gmail.com>
2023-07-16 21:53:06 +0200

    moved types to other file

--- src/features/movies/MovieList/MovieList.tsx
+++ src/features/movies/MovieList/MovieList.tsx
@@ -6,14 +6,11 @@ import { useQuery } from "@tanstack/react-query";
 import { getGenres, getMovieList } from "./getMovieList.ts";
 import Loading from "../../../common/Loading/Loading.tsx";
 import ErrorPage from "../../../common/ErrorPage/ErrorPage.tsx";
+import { getGenreName } from "../../../common/utils/getGenresName.ts";
 
 const MovieList = () => {
 	const {isLoading, data} = useQuery(["movies"], getMovieList);
 	const genresQuery = useQuery(["genres"], getGenres);
-	const getGenreName = (genreId: number) => {
-		const genre = genresQuery.data.genres.find((genre:{id: number, name: string}) => genre.id === genreId);
-		return genre ? genre.name : "";
-	};
 
 	if (isLoading) return <Loading/>;
 
@@ -25,7 +22,7 @@ const MovieList = () => {
 			      <Tile
 				      title={movie.title}
 				      date={movie.release_date.slice(0, 4)}
-				      genres={movie.genre_ids.map((genreId) => `${getGenreName(genreId)}`)}
+				      genres={movie.genre_ids.map((genreId) => `${getGenreName(genresQuery.data, genreId)}`)}
 				      note={movie.vote_average}
 				      votes={movie.vote_count}
 				      imageUrl={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : undefined}
