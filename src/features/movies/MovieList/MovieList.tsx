a244cc7b4f4665fe60039e5376a74a6ce27cf5d0
GrzegorzJendernal <g.jendernal@gmail.com>
2023-07-22 21:59:56 +0200

    fixed whitespaces, added attribute to queryKey

--- src/features/movies/MovieList/MovieList.tsx
+++ src/features/movies/MovieList/MovieList.tsx
@@ -7,34 +7,39 @@ import { getGenres, getMovieList } from "./getMovieList.ts";
 import Loading from "../../../common/Loading/Loading.tsx";
 import ErrorPage from "../../../common/ErrorPage/ErrorPage.tsx";
 import { getGenreName } from "../../../common/utils/getGenresName.ts";
+import { useState } from "react";
 
 const MovieList = () => {
-	const {isLoading, data} = useQuery(["movies"], getMovieList);
-	const genresQuery = useQuery(["genres"], getGenres);
+  const [page, setPage] = useState(1);
+  const { isLoading, data } = useQuery(["movies", page], () => getMovieList(page));
+  const genresQuery = useQuery(["genres"], getGenres);
 
-	if (isLoading) return <Loading/>;
+  if (isLoading) return <Loading />;
 
-  if (data) return (
-    <PageWrapper>
-      <PageHeading>Popular movies</PageHeading>
-      <Container>
-	      {data.results.map(movie => (
-			      <Tile
-				      title={movie.title}
-				      date={movie.release_date.slice(0, 4)}
-				      genres={genresQuery.data && movie.genre_ids.map((genreId) => `${getGenreName(genresQuery.data, genreId)}`)}
-				      note={movie.vote_average}
-				      votes={movie.vote_count}
-				      imageUrl={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : undefined}
-				      key={movie.id}
-			      />
-		      ))}
-      </Container>
-	    <Pagination/>
-    </PageWrapper>
-  );
+  if (data)
+    return (
+      <PageWrapper>
+        <PageHeading>Popular movies</PageHeading>
+        <Container>
+          {data.results.map((movie) => (
+            <Tile
+              title={movie.title}
+              date={movie.release_date.slice(0, 4)}
+              genres={
+                genresQuery.data && movie.genre_ids.map((genreId) => `${getGenreName(genresQuery.data, genreId)}`)
+              }
+              note={movie.vote_average}
+              votes={movie.vote_count}
+              imageUrl={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : undefined}
+              key={movie.id}
+            />
+          ))}
+        </Container>
+        <Pagination page={page} setPage={setPage} totalPages={data.total_pages} />
+      </PageWrapper>
+    );
 
-	return <ErrorPage/>
+  return <ErrorPage />;
 };
 
 export default MovieList;
