e4d19f7a6e78b15debce1eb628ff5dc1750f4806
GrzegorzJendernal <g.jendernal@gmail.com>
2023-07-24 21:35:57 +0200

    fixed whitespaces

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			staleTime: 1000 * 60 * 5,
-		},
-	},
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-	  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-		  <ReactQueryDevtools/>
-	  </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
