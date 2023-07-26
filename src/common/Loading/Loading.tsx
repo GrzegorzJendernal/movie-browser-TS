2ffb664f2964c6cbf8fb104ea6d7a74b378320b1
GrzegorzJendernal <g.jendernal@gmail.com>
2023-07-26 21:02:37 +0200

    fixed Loading

--- src/common/Loading/Loading.tsx
+++ src/common/Loading/Loading.tsx
@@ -1,10 +1,12 @@
 import { PageHeading, PageWrapper } from "../page/page.styled";
+import { useQueryParam } from "../utils/useQueryParam";
 import { Spinner, SpinnerBox } from "./loading.styled";
 
 const Loading = () => {
+  const query = useQueryParam("search");
   return (
     <PageWrapper>
-      <PageHeading>Search results for “Mulan”</PageHeading>
+      {query !== "" && <PageHeading>Search results for “{query}”</PageHeading>}
       <SpinnerBox>
         <Spinner />
       </SpinnerBox>
