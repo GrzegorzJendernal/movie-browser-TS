24198b69a8a7151da29c29ef220acdc1fd8fe65f
GrzegorzJendernal <g.jendernal@gmail.com>
2023-08-02 21:20:41 +0200

    added test functionality to component

--- src/features/movies/MovieDetails/MovieDetails.tsx
+++ src/features/movies/MovieDetails/MovieDetails.tsx
@@ -1,9 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import { useIdFromUrl } from "../../../common/utils/useIdFromUrl";
+import { getMovieDetails } from "../../../common/api/apiRequests";
+import ErrorPage from "../../../common/ErrorPage/ErrorPage";
+import Loading from "../../../common/Loading/Loading";
 
 const MovieDetails = () => {
   const id = useIdFromUrl();
+  const { data, isLoading } = useQuery(["movie"], () => getMovieDetails(id));
 
-  return <div>sialalala {id}</div>;
+  if (isLoading) return <Loading />;
+
+  if (data) return <div>{data.title}</div>;
+
+  return <ErrorPage />;
 };
 
 export default MovieDetails;
