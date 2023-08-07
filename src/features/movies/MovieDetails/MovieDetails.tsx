3d497f9e76959ae70759fac422c3078f2102eadd
GrzegorzJendernal <g.jendernal@gmail.com>
2023-08-07 21:32:54 +0200

    added backdrop

--- src/features/movies/MovieDetails/MovieDetails.tsx
+++ src/features/movies/MovieDetails/MovieDetails.tsx
@@ -3,6 +3,8 @@ import { useIdFromUrl } from "../../../common/utils/useIdFromUrl";
 import { getMovieDetails } from "../../../common/api/apiRequests";
 import ErrorPage from "../../../common/ErrorPage/ErrorPage";
 import Loading from "../../../common/Loading/Loading";
+import { PageWrapper } from "../../../common/page/page.styled";
+import { Backdrop, Poster } from "./movieDetails.styled";
 
 const MovieDetails = () => {
   const id = useIdFromUrl();
@@ -10,7 +12,15 @@ const MovieDetails = () => {
 
   if (isLoading) return <Loading />;
 
-  if (data) return <div>{data.title}</div>;
+  if (data)
+    return (
+      <>
+        <Backdrop>
+          <Poster imageUrl={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}></Poster>
+        </Backdrop>
+        <PageWrapper></PageWrapper>
+      </>
+    );
 
   return <ErrorPage />;
 };
