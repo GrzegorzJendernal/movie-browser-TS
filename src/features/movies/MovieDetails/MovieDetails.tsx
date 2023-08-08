48e1fbfd099df6a1987634370b52ceb567625f4d
GrzegorzJendernal <g.jendernal@gmail.com>
2023-08-08 21:22:58 +0200

    added styles to info, unfunished

--- src/features/movies/MovieDetails/MovieDetails.tsx
+++ src/features/movies/MovieDetails/MovieDetails.tsx
@@ -4,7 +4,17 @@ import { getMovieDetails } from "../../../common/api/apiRequests";
 import ErrorPage from "../../../common/ErrorPage/ErrorPage";
 import Loading from "../../../common/Loading/Loading";
 import { PageWrapper } from "../../../common/page/page.styled";
-import { Backdrop, Poster } from "./movieDetails.styled";
+import {
+  Backdrop,
+  BigNote,
+  BigRating,
+  BigStar,
+  BigTitle,
+  MainInfo,
+  OutOf,
+  Poster,
+  VotesAmount,
+} from "./movieDetails.styled";
 
 const MovieDetails = () => {
   const id = useIdFromUrl();
@@ -16,7 +26,17 @@ const MovieDetails = () => {
     return (
       <>
         <Backdrop>
-          <Poster imageUrl={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}></Poster>
+          <Poster imageUrl={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}>
+            <MainInfo>
+              <BigTitle>{data.title}</BigTitle>
+              <BigRating>
+                <BigStar />
+                <BigNote>{data.vote_average}</BigNote>
+                <OutOf>/ 10</OutOf>
+                <VotesAmount>{data.vote_count} votes</VotesAmount>
+              </BigRating>
+            </MainInfo>
+          </Poster>
         </Backdrop>
         <PageWrapper></PageWrapper>
       </>
