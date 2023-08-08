81e1b4f5d0b7e87d3fecf89f0c4a9b577ad15d1b
GrzegorzJendernal <g.jendernal@gmail.com>
2023-08-08 20:46:31 +0200

    added @media to poster

--- src/features/movies/MovieDetails/movieDetails.styled.ts
+++ src/features/movies/MovieDetails/movieDetails.styled.ts
@@ -5,14 +5,28 @@ export const Backdrop = styled.div`
   width: 100%;
   height: 769px;
   background-color: ${({ theme }) => theme.colors.backdrop};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mediumDesktop}) {
+    height: unset;
+  }
 `;
 
 export const Poster = styled.div<PictureProps>`
-  margin: 0 200px;
+  margin: 0 auto;
+  width: 1368px;
   height: 100%;
   background-position: center;
   background-size: 100%, contain;
   background-repeat: no-repeat;
   background-image: ${({ imageUrl }) => `url(${imageUrl})`};
   box-shadow: rgb(0, 0, 0) 0px 0px 75px 75px inset;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mediumDesktop}) {
+    aspect-ratio: 1.78;
+    width: auto;
+    height: auto;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    box-shadow: rgb(0, 0, 0) 0px 0px 30px 30px inset;
+  }
 `;
