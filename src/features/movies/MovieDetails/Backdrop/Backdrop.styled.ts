deb776ba421d75777196986d9d018d41b637bec5
GrzegorzJendernal <g.jendernal@gmail.com>
2023-08-21 11:30:58 +0200

    added @media to Backdrop

--- src/features/movies/MovieDetails/Backdrop/Backdrop.styled.ts
+++ src/features/movies/MovieDetails/Backdrop/Backdrop.styled.ts
@@ -42,6 +42,33 @@ export const MainInfo = styled.div`
   color: ${({ theme }) => theme.colors.base};
   line-height: 1.2;
   margin-bottom: 56px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mediumDesktop}) {
+    margin-left: 56px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    gap: 20px;
+    margin-bottom: 46px;
+    margin-left: 46px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    gap: 16px;
+    margin-bottom: 36px;
+    margin-left: 30px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    gap: 10px;
+    margin-bottom: 10px;
+    margin-left: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tiny}) {
+    gap: 5px;
+    margin-bottom: 8px;
+  }
 `;
 
 export const BigTitle = styled.h2`
@@ -49,12 +76,34 @@ export const BigTitle = styled.h2`
   font-style: normal;
   font-weight: 600;
   margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    font-size: 54px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 44px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 34px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tiny}) {
+    font-size: 24px;
+  }
 `;
 
 export const BigRating = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    gap: 8px;
+    flex-direction: row;
+    align-items: center;
+  }
 `;
 
 export const NotesWrapper = styled.div`
@@ -65,7 +114,23 @@ export const NotesWrapper = styled.div`
 
 export const BigStar = styled(Star)`
   width: 40px;
-  height: 40px;
+  height: auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    width: 33px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    width: 24px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    width: 20px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tiny}) {
+    width: 16px;
+  }
 `;
 
 export const BigNote = styled.span`
@@ -73,17 +138,62 @@ export const BigNote = styled.span`
   font-style: normal;
   font-weight: 500;
   line-height: 1.3;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    font-size: 26px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 22px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 18px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tiny}) {
+    font-size: 14px;
+  }
 `;
 
 export const OutOf = styled.span`
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
-  padding-top: 10px;
+  padding-top: 8px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 14px;
+    padding-top: 7px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 12px;
+    padding-top: 6px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tiny}) {
+    font-size: 10px;
+    padding-top: 5px;
+  }
 `;
 
 export const VotesAmount = styled.div`
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 14px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 12px;
+    padding-top: 6px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tiny}) {
+    font-size: 10px;
+    padding-top: 5px;
+  }
 `;
