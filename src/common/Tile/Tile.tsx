9c07ed498608492b5b002bf290b163f6264910e9
GrzegorzJendernal <g.jendernal@gmail.com>
2023-08-28 11:53:32 +0200

    added Data div to Tile

--- src/common/Tile/Tile.tsx
+++ src/common/Tile/Tile.tsx
@@ -12,6 +12,7 @@ import {
   Votes,
   StyledLink,
   Overview,
+  Data,
 } from "./tile.styled";
 
 interface TileProps {
@@ -27,18 +28,20 @@ interface TileProps {
 
 const Tile = ({ title, date, genres, note, votes, imageUrl, path, overview }: TileProps) => {
   const tileContent = (
-    <StyledTile>
+    <StyledTile description={path ? true : false}>
       <Picture imageUrl={imageUrl} />
-      <Info>
-        <Title>{title}</Title>
-        <Additional>{date}</Additional>
-        <Tags>{!!genres && genres.map((genre) => <Tag key={genre}>{genre}</Tag>)}</Tags>
-      </Info>
-      <Rates>
-        <StyledStar />
-        <Note>{note.toFixed(1)}</Note>
-        <Votes>{votes} votes</Votes>
-      </Rates>
+      <Data>
+        <Info>
+          <Title>{title}</Title>
+          <Additional>{date}</Additional>
+          <Tags>{!!genres && genres.map((genre) => <Tag key={genre}>{genre}</Tag>)}</Tags>
+        </Info>
+        <Rates>
+          <StyledStar />
+          <Note>{note.toFixed(1)}</Note>
+          <Votes>{votes} votes</Votes>
+        </Rates>
+      </Data>
       {!!overview && <Overview>{overview}</Overview>}
     </StyledTile>
   );
