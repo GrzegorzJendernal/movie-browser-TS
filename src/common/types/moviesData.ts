20a316c4ea0778b72a4a91d676e60f6fe9bc3e13
GrzegorzJendernal <g.jendernal@gmail.com>
2023-08-30 11:36:19 +0200

    added type of countires

--- src/common/types/moviesData.ts
+++ src/common/types/moviesData.ts
@@ -13,6 +13,7 @@ export interface Movie {
   video: boolean;
   vote_average: number;
   vote_count: number;
+  production_countries?: [{ iso_3166_1: string; name: string }];
 }
 
 export interface MovieListData {
