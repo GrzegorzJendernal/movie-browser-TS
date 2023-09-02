c7bf1d0af98e3a379a90c2fe71035d9d2c559a02
GrzegorzJendernal <g.jendernal@gmail.com>
2023-09-02 16:11:51 +0200

    added another prop

--- src/common/types/styledProps.ts
+++ src/common/types/styledProps.ts
@@ -2,10 +2,11 @@ export interface PictureProps {
   imageUrl?: string;
 }
 
-export interface DescriptionProps {
-  description?: boolean;
+export interface DetailsProps {
+  details?: boolean;
 }
 
 export interface StyledTileProps {
   movie?: boolean;
+  hideOnMobile?: boolean;
 }
