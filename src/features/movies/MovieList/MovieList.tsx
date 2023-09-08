8baf5a03b8e8523b66942b83b15b1d1dd79df5aa
GrzegorzJendernal <g.jendernal@gmail.com>
2023-09-08 21:30:09 +0200

    refactored MovieList

--- src/features/movies/MovieList/MovieList.tsx
+++ src/features/movies/MovieList/MovieList.tsx
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { getMovieList, getMoviesByQuery } from "../../../common/api/apiRequests.ts";
 import Loading from "../../../common/Loading/Loading.tsx";
 import ErrorPage from "../../../common/ErrorPage/ErrorPage.tsx";
-import NoResults from "../../../common/NoResults/NoResults.tsx";
 import { useQueryParam } from "../../../common/utils/useQueryParam.ts";
 import Movies from "./Movies/Movies.tsx";
 
@@ -10,15 +9,15 @@ const MovieList = () => {
   const pageParam = useQueryParam("page");
   const page = pageParam === "" ? 1 : +pageParam;
   const query = useQueryParam("search");
-  const { isLoading, data } = useQuery(["movies", page, query], () =>
+  const { isLoading, isError } = useQuery(["movies", page, query], () =>
     query === "" ? getMovieList(page) : getMoviesByQuery(page, query),
   );
 
   if (isLoading) return <Loading />;
 
-  if (data) return data.total_results === 0 ? <NoResults query={query} /> : <Movies />;
+  if (isError) return <ErrorPage />;
 
-  return <ErrorPage />;
+  return <Movies />;
 };
 
 export default MovieList;
