768d17848311ec75fc70483bd1ed39435e5fbaa4
GrzegorzJendernal <g.jendernal@gmail.com>
2023-09-09 10:05:35 +0200

    added logic to PeopleList

--- src/features/people/PeopleList/PeopleList.tsx
+++ src/features/people/PeopleList/PeopleList.tsx
@@ -1,6 +1,22 @@
-const PeopleList = () => {
+import { useQuery } from "@tanstack/react-query";
+import { useQueryParam } from "../../../common/utils/useQueryParam";
+import { getPeopleList, getPersonByQuery } from "../../../common/api/apiRequests";
+import Loading from "../../../common/Loading/Loading";
+import ErrorPage from "../../../common/ErrorPage/ErrorPage";
+import People from "./People/People";
 
-  return (
-    
+const PeopleList = () => {
+  const pageParam = useQueryParam("page");
+  const page = pageParam === "" ? 1 : +pageParam;
+  const query = useQueryParam("search");
+  const { isLoading, isError } = useQuery(["people", page, query], () =>
+    query === "" ? getPeopleList(page) : getPersonByQuery(page, query),
   );
+
+  if (isLoading) return <Loading />;
+  if (isError) return <ErrorPage />;
+
+  return <People />;
 };
+
+export default PeopleList;
