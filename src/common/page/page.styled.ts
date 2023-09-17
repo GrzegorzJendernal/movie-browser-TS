402da46d9102873c657a69c015093ba290cee724
GrzegorzJendernal <g.jendernal@gmail.com>
2023-09-17 20:26:51 +0200

    moved container to page.styled and renamed to PageSection

--- src/common/page/page.styled.ts
+++ src/common/page/page.styled.ts
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { PageSectionProps } from "../types/styledProps";
 
 export const PageWrapper = styled.div`
   max-width: 1368px;
@@ -34,4 +35,35 @@ export const PageHeading = styled.h1`
   @media (max-width: ${({ theme }) => theme.breakpoints.tiny}) {
     font-size: 18px;
   }
+`;
+
+export const PageSection = styled.section<PageSectionProps>`
+  ${({ contents }) =>
+    contents === "movies" &&
+    css`
+      margin-top: 24px;
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      grid-gap: 24px;
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 24px;
+      }
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 24px;
+      }
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 24px;
+      }
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        grid-template-columns: 1fr;
+        grid-gap: 16px;
+      }
+    `}
 `;
