dfe8dd55a2f3a6a0ff90e3fe8080750247e4f910
GrzegorzJendernal <g.jendernal@gmail.com>
2023-09-22 21:15:23 +0200

    refactored PersonDetails

--- src/features/people/PersonDetails/PersonDetails.tsx
+++ src/features/people/PersonDetails/PersonDetails.tsx
@@ -3,16 +3,17 @@ import { useIdFromUrl } from "../../../common/utils/useIdFromUrl";
 import { getPersonDetails } from "../../../common/api/apiRequests";
 import Loading from "../../../common/Loading/Loading";
 import ErrorPage from "../../../common/ErrorPage/ErrorPage";
+import Person from "./Person/Person";
 
 const PersonDetails = () => {
   const id = useIdFromUrl();
-  const { data, isLoading } = useQuery(["person", id], () => getPersonDetails(id));
+  const { isError, isLoading } = useQuery(["person", id], () => getPersonDetails(id));
 
   if (isLoading) return <Loading />;
 
-  if (data) return <>sialala</>;
+  if (isError) return <ErrorPage />;
 
-  return <ErrorPage />;
+  return <Person />;
 };
 
 export default PersonDetails;
