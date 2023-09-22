36d1e6c0193bfc9d5f3a46508a69c847cd608d26
GrzegorzJendernal <g.jendernal@gmail.com>
2023-09-22 21:48:57 +0200

    added Person component - unfinished

---
+++ src/features/people/PersonDetails/Person/Person.tsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { useIdFromUrl } from "../../../../common/utils/useIdFromUrl";
+import { getPersonDetails } from "../../../../common/api/apiRequests";
+import ErrorPage from "../../../../common/ErrorPage/ErrorPage";
+import { PageWrapper } from "../../../../common/page/page.styled";
+import Tile from "../../../../common/Tile/Tile";
+
+const Person = () => {
+  const id = useIdFromUrl();
+  const { data } = useQuery(["person", id], () => getPersonDetails(id));
+
+  if (!data) return <ErrorPage />;
+
+  return (
+    <PageWrapper details>
+      <Tile title={data.name} person birthdate={data.birthday} birthplace={data.place_of_birth} />
+    </PageWrapper>
+  );
+};
+
+export default Person;
