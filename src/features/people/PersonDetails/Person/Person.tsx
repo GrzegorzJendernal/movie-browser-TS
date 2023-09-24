5ec10f0ec33086cc4fe22ea13e7578647c93e585
GrzegorzJendernal <g.jendernal@gmail.com>
2023-09-24 21:42:48 +0200

    added cast section

--- src/features/people/PersonDetails/Person/Person.tsx
+++ src/features/people/PersonDetails/Person/Person.tsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { useIdFromUrl } from "../../../../common/utils/useIdFromUrl";
-import { getPersonDetails } from "../../../../common/api/apiRequests";
+import { getGenres, getMovieCredits, getPersonDetails } from "../../../../common/api/apiRequests";
 import ErrorPage from "../../../../common/ErrorPage/ErrorPage";
-import { PageWrapper } from "../../../../common/page/page.styled";
+import { PageHeading, PageSection, PageWrapper } from "../../../../common/page/page.styled";
 import Tile from "../../../../common/Tile/Tile";
+import { getGenreName } from "../../../../common/utils/getGenresName";
 
 const Person = () => {
   const id = useIdFromUrl();
   const { data } = useQuery(["person", id], () => getPersonDetails(id));
+  const credits = useQuery(["movieCredits", id], () => getMovieCredits(id));
+  const genresQuery = useQuery(["genres"], getGenres);
 
   if (!data) return <ErrorPage />;
 
@@ -21,6 +24,31 @@ const Person = () => {
         overview={data.biography}
         imageUrl={data.profile_path ? `https://image.tmdb.org/t/p/w500/${data.profile_path}` : undefined}
       />
+      {!!credits.data && !!credits.data.cast && (
+        <>
+          <PageHeading details as={"h2"}>
+            Movies cast ({credits.data.cast.length})
+          </PageHeading>
+          <PageSection contents={"movies"}>
+            {credits.data.cast.map((movie) => (
+              <Tile
+                path={`/movies/${movie.id}`}
+                title={movie.title}
+                date={movie.release_date.slice(0, 4)}
+                genres={
+                  genresQuery.data && movie.genre_ids.map((genreId) => `${getGenreName(genresQuery.data, genreId)}`)
+                }
+                note={movie.vote_average}
+                votes={movie.vote_count}
+                imageUrl={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : undefined}
+                key={movie.credit_id}
+                job={movie.character}
+                movies
+              />
+            ))}
+          </PageSection>
+        </>
+      )}
     </PageWrapper>
   );
 };
