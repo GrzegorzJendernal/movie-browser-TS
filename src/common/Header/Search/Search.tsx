1a1820e5b9610d63d71a9b585a6021627e67043c
GrzegorzJendernal <g.jendernal@gmail.com>
2023-09-25 21:12:23 +0200

    fixed search engine

--- src/common/Header/Search/Search.tsx
+++ src/common/Header/Search/Search.tsx
@@ -1,13 +1,21 @@
+import { useEffect } from "react";
 import { useReplaceQueryParameter } from "../../utils/queryParameters";
 import { useQueryParam } from "../../utils/useQueryParam";
 import { Input, Label, Loupe } from "./search.styled";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Search = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const query = useQueryParam("search");
   const replaceQueryParameter = useReplaceQueryParameter();
   const setQuery = ({ target }: { target: { value: string } }) => {
+    if (location.pathname.includes("movies")) {
+      navigate({ pathname: "/movies" });
+    }
+    if (location.pathname.includes("people")) {
+      navigate({ pathname: "/people" });
+    }
     replaceQueryParameter({
       key: "search",
       value: target.value.trim() !== "" ? target.value : "",
